ca47948f6870415dc252a0d30e14bcfc319373b1
VitorConteCesul <vitor.2024014338@alunocesul.com>
2025-03-31 17:27:45 -0300

    atualizando aulas

---
+++ maratonaJava/src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/calculadoraTest03.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.javacore.Bintroducaometodos.test;
+
+import academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.Calculadora;
+
+public class calculadoraTest03 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+
+        calculadora.multiplicarDoisNumeros(43, 65);
+    }
+}
